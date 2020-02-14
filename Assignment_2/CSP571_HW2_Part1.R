28d533a6a4de92a5c437185774b9a9260853ed04
rahulmnair1997 <rahulmnair1997@gmail.com>
2020-02-14 10:36:21 -0600

    assignment upload

---
+++ Assignment_2/CSP571_HW2_Part1.R
@@ -0,0 +1,89 @@
+# CSP/MATH 571 Homework 2 Part 1
+# Enter your email below
+yourEmail<- "rnair14@hawk.iit.edu"
+
+# Load in the Boston Housing data set using the code below.
+#install.packages('mlbench')
+library('mlbench')
+data(BostonHousing)
+
+
+# 1. Create a scatterplot matrix of all variables in the data set. Save your output.
+
+pairs(x = d, main = "Scatter plot matrix")
+
+
+
+# 2. For each numeric variable in BostonHousing, create a separate boxplot using
+# "Method 2" listed in the class notes. Do this programmatically; meaning do
+# not simply hardcode the creation of every boxplot. Instead, loop over the
+# approriate columns and create the boxplots. Save your output. Ensure your boxplots
+# all have proper titles
+
+indices <- which(sapply(BostonHousing, is.numeric))
+# columns <- names(BostonHousing)[which(sapply(BostonHousing, is.numeric))]
+columns <- names(BostonHousing)[which(sapply(BostonHousing, is.numeric))]
+columns 
+for (i in indices){
+  boxplot(x = BostonHousing[i], main = )
+}
+for ( i in indices){
+  print(columns[i])
+}
+indices
+# 3. Create a correlation matrix and correlation plot
+# for the BostonHousing data set. Save your output.
+
+
+
+
+# 4. Identify the top 3 strongest absolute correlations in the data set. Save your output.
+
+
+
+
+
+# 5. Create a new variable call ageGroup quartiles. Divide the age variable
+# into four even sections and assign it to one quartile.
+
+
+
+
+
+# 6. Go to the website listed below. Convert the html table into a
+# dataframe with columns NO, Player, Highlights
+library('rvest')
+library('tidyr')
+url = 'http://www.espn.com/nfl/superbowl/history/mvps'
+
+
+
+
+# 7.Extract the names of the MVPs, Position and Team into columns
+# MVP1, MVP2, Position, Team
+
+
+
+
+
+# 8. Determine the 90th%, 92.5th%, 95th%, 97.5th% and 99th% confidence intervals
+# for the mean of passing yards
+# (as listed in "Highlights" column) for quarterbacks.
+# Note that there are a few intermediate steps you'll probably want to do
+# in order to accomplish this. I am purposelly leaving that up to you, so that
+# you are starting to develop the types of problem solving skills you'll need
+# to tackle these problems in the wild.
+
+
+
+# 9. The following contains data on the calorie counts of four types
+# of foods. Perform an ANOVA and determine the Pr(>F)
+food1 <- c(164,   172,   168,   177, 	156, 	195)
+food2 <- c(178,   191, 	197, 	182, 	185, 	177)
+food3 <- c(175, 	193, 	178, 	171, 	163, 	176)
+food4 <- c(155, 	166, 	149, 	164, 	170, 	168)
+
+
+# 10. Determine how many
+# Tuesdays fell on the first of the month
+# during the 19th century (1 Jan 1801 to 31 Dec 1901).
